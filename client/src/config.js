cc3a6f3d4aadf5bf96ea736f9c13b7e3c7a46c6e
omerchernia <omer12899@gmail.com>
2024-10-16 16:47:55 +0300

    fix url

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,2 @@
-export const API_BASE_URL = 'https://fascinating-seahorse-c7f0dd.netlify.app/'; // Adjust this URL as needed
+export const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+
